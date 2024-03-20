c8dc08fc33b65fe056be183594fd42718cf8a647
James Andrade Cevallos <james0jesus@gmail.com>
2024-03-20 23:49:58 +0100

    Controlador Jugadores Funciona

---
+++ Proyecto10AbrilBack/Controllers/JugadoresController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proyecto10AbrilBack.Data;
+using ProyectoCore10AbrilBack.Models;
+
+namespace Proyecto10AbrilBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JugadoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JugadoresController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Jugadores
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Jugador>>> GetJugadores()
+        {
+            return await _context.Jugadores.ToListAsync();
+        }
+
+        // GET: api/Jugadores/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Jugador>> GetJugador(int id)
+        {
+            var jugador = await _context.Jugadores.FindAsync(id);
+
+            if (jugador == null)
+            {
+                return NotFound();
+            }
+
+            return jugador;
+        }
+
+        // PUT: api/Jugadores/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutJugador(int id, Jugador jugador)
+        {
+            if (id != jugador.JugadorId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(jugador).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!JugadorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Jugadores
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Jugador>> PostJugador(Jugador jugador)
+        {
+            _context.Jugadores.Add(jugador);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetJugador", new { id = jugador.JugadorId }, jugador);
+        }
+
+        // DELETE: api/Jugadores/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteJugador(int id)
+        {
+            var jugador = await _context.Jugadores.FindAsync(id);
+            if (jugador == null)
+            {
+                return NotFound();
+            }
+
+            _context.Jugadores.Remove(jugador);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool JugadorExists(int id)
+        {
+            return _context.Jugadores.Any(e => e.JugadorId == id);
+        }
+    }
+}
